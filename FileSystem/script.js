71e1399942676c9fa792f52c406849bf05c14be2
Anikesh Roy <anikeshroy62040@gmail.com>
2026-08-11 15:32:57 +0530

    learnt: writeFile, readFile, appendFile & rename

--- FileSystem/script.js
+++ FileSystem/script.js
@@ -1,10 +1,10 @@
 const { log } = require('node:console')
 const fs = require('node:fs')
 
-const content = " This is append file creation using node js"
+const content = "Test file creation using writeFile"
 
 // ------------------- Creating File Asynchronosly 
-fs.writeFile("test.txt", content, (err) => {
+fs.writeFile("./test.txt", content, (err) => {
     if (err) {
         console.log(err);
     } else {
@@ -13,9 +13,21 @@ fs.writeFile("test.txt", content, (err) => {
 })
 
 // ------------------- Adding Content in created file or existing file synchronosly
+const updatedContent = " This is the updated content"
 try {
-    fs.appendFileSync('test.txt', content)
+    fs.appendFileSync('test.txt', updatedContent)
     console.log("File Updated");
 } catch (err) { console.log(err) };
 
-fs.readFile("test.txt", content, r)
+//-------------------- Reading file data using readfile
+fs.readFile("./test.txt", 'utf-8', (data, err) => {
+    if (err) { console.error(err); }
+    console.log(data)
+})
+
+fs.rename("newTest.html", "test.txt", (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log("file Renamed")
+})
