83a01b3604900ce0b87f7f9984e474dcdf7c0fcf
Anikesh Roy <anikeshroy62040@gmail.com>
2026-08-12 12:33:32 +0530

    feat: initialize MongoDB project with user model and dependencies - Added userModel.js to define the user schema for MongoDB connection.

---
+++ 05_MongoDB/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const userModel = require('./userModel')
+const { model } = require('mongoose')
+
+app.get('/', (req, res) => {
+    res.end("Hello From Server")
+})
+
+app.get('/create', async (req, res) => {
+    let createdUser = await userModel.create({
+        name: "Samay Raina",
+        userName: "samay343",
+        email: "rainasamay@gmail.com"
+    })
+
+    console.log(createdUser);
+    res.send(createdUser)
+})
+
+app.get('/update', async (req, res) => {
+    let updatedUser = await userModel.findOneAndUpdate({ userName: "aniraj343" }, { name: "Ani Raj" }, { new: true })
+
+    console.log(updatedUser);
+    res.send(updatedUser)
+})
+
+app.get('/read', async (req, res) => {
+    let allUser = await userModel.find()
+
+    console.log(allUser);
+    res.send(allUser)
+})
+
+app.listen(PORT, () => {
+    console.log("Server is Running on", `localhost:${PORT}`);
+})
