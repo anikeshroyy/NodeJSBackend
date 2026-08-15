9ad62ffd0af7aa438c468ee0d541ab84c310621a
Anikesh Roy <anikeshroy62040@gmail.com>
2026-08-15 18:54:35 +0530

    feat: update database connection to use environment variable for MongoDB URI

--- 10_MiniProject/models/DB_config.js
+++ 10_MiniProject/models/DB_config.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(`mongodb://localhost:27017/Vibely`)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDb connected Successfuly");
     } catch (error) {
         console.error(err.message);
     }
 }
-
 
 module.exports = connectDb;
