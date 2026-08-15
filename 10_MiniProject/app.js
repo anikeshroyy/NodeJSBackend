1b97016acaccf1f1c508394339b1620730372557
Anikesh Roy <anikeshroy62040@gmail.com>
2026-08-15 07:55:02 +0530

    feat: add user profile editing functionality and enhance user model with username, bio, and profile picture

--- 10_MiniProject/app.js
+++ 10_MiniProject/app.js
@@ -49,7 +49,8 @@ app.post('/signup', async (req, res) => {
                 let newUser = await userModel.create({
                     name: req.body.fullName,
                     password: hash,
-                    email: req.body.email
+                    email: req.body.email,
+                    username: req.body.username,
                 })
 
                 console.log("User is created");
@@ -76,7 +77,7 @@ app.post('/login', async (req, res) => {
                 return res.send("Either email or password is incorrect")
             }
 
-            let token = jwt.sign({ email: loggedInUser.email }, "secrets")
+            let token = jwt.sign({ id: loggedInUser._id, email: loggedInUser.email }, "secrets")
             res.cookie("token", token)
             console.log(result);
             // res.send("You are logged in")
@@ -99,13 +100,50 @@ app.get('/profile', async (req, res) => {
 
         const validUser = jwt.verify(token, "secrets")
 
-        const user = await userModel.findOne({ email: validUser.email })
+        const user = await userModel.findOne({ _id: validUser.id })
 
         res.render("userProfile", { user })
 
     } catch (error) {
         console.error(error.message);
     }
+})
+
+app.get('/profile/edit', async (req, res) => {
+
+    let token = await req.cookies.token;
+
+    let user = await jwt.verify(token, "secrets")
+
+    let editUser = await userModel.findOne({ _id: user.id })
+
+    res.render("editProfile", { user: editUser })
+})
+
+app.post('/profile/edit', async (req, res) => {
+    try {
+
+        let { name, email, profilePicture, bio } = req.body
+
+        let token = await req.cookies.token;
+        let user = await jwt.verify(token, "secrets")
+
+        let updatedUser = await userModel.findOneAndUpdate({ _id: user.id }, {
+            name,
+            email,
+            profilePicture,
+            bio,
+        })
+
+        res.redirect('/profile')
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+app.get('/logout', async (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/");
 })
 
 app.listen(PORT, () => {
--- 10_MiniProject/models/user.js
+++ 10_MiniProject/models/user.js
@@ -1,9 +1,46 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50
+    },
+
+    username: {
+        type: String,
+        required: true,
+        // unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 20
+    },
+
+    email: {
+        type: String,
+        required: true,
+        // unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+
+    bio: {
+        type: String,
+        maxlength: 160,
+        default: ""
+    },
 })
 
 module.exports = mongoose.model("user", userSchema)
